160b6e99af250442cfd795479d088a6f5887962e
Thomas <contact@thomas-george.com>
2021-03-13 16:13:22 +0100

    feat: creation and centring of the ball

--- main.lua
+++ main.lua
@@ -10,7 +10,16 @@ padRight.positionY = 519
 padRight.width = 10
 padRight.height = 80
 
+ball = {}
+ball.positionX = 0
+ball.positionY = 0
+ball.width = 10
+ball.height = 10
+
 function love.load()
+  -- centring of the ball
+  ball.positionX = (love.graphics.getWidth() / 2) - (ball.width / 2)
+  ball.positionY = (love.graphics.getHeight() / 2) - (ball.height / 2)
 end
 
 function love.update(dt)
@@ -28,4 +37,6 @@ end
 function love.draw()
   love.graphics.rectangle('fill', padLeft.positionX, padLeft.positionY, padLeft.width, padLeft.height)
   love.graphics.rectangle('fill', padRight.positionX, padRight.positionY, padRight.width, padRight.height)
+  
+  love.graphics.circle('fill', ball.positionX, ball.positionY, ball.width, ball.height)
 end
