26e9055aae3eed97af43f878d7f92fe58c00f58b
Thomas <contact@thomas-george.com>
2021-03-14 14:48:44 +0100

    feat: add of second player / refactor: merge of collisions function

--- main.lua
+++ main.lua
@@ -3,18 +3,33 @@ require "ball"
 
 
 function love.load()
+  padLeft.positionX = 0
+  padLeft.positionY = 0
+  
+  padRight.positionX = love.graphics.getWidth() - padRight.width
+  padRight.positionY = love.graphics.getHeight() - padRight.height
+  
   ballCentring()
 end
 
 
 function love.update(dt)
-  -- moving pads
-  if love.keyboard.isDown('up') and padLeft.positionY > 0 then
-    padLeft.positionY = padLeft.positionY - 4
+  -- moving left pad
+  if love.keyboard.isDown('a') and padLeft.positionY > 0 then
+    padLeft.positionY = padLeft.positionY - padLeft.speedY
   end
   
-  if love.keyboard.isDown('down') and (padLeft.positionY + padLeft.height) < love.graphics.getHeight() then
-    padLeft.positionY = padLeft.positionY + 4
+  if love.keyboard.isDown('q') and (padLeft.positionY + padLeft.height) < love.graphics.getHeight() then
+    padLeft.positionY = padLeft.positionY + padLeft.speedY
+  end
+  
+  -- moving right pad
+  if love.keyboard.isDown('up') and padRight.positionY > 0 then
+    padRight.positionY = padRight.positionY - padRight.speedY
+  end
+  
+  if love.keyboard.isDown('down') and (padRight.positionY + padRight.height) < love.graphics.getHeight() then
+    padRight.positionY = padRight.positionY + padRight.speedY
   end
   
   -- ball movement
@@ -22,30 +37,30 @@ function love.update(dt)
   ball.positionX = ball.positionX + ball.speedX
   
   
-  -- collisions management with the 4 walls
-  if ball.positionY > (love.graphics.getHeight() - ball.height) then
+  -- collisions management with top and bottom walls
+  if ball.positionY <= 0 or (ball.positionY + ball.height) >= love.graphics.getHeight() then
     ball.speedY = -ball.speedY
   end
   
-  if ball.positionY <= 0 then
-    ball.speedY = -ball.speedY
-  end
-    
-  if ball.positionX > (love.graphics.getWidth() - ball.width) then
-    ball.speedX = -ball.speedX
-  end
-  
-  if ball.positionX <= 0 then
+  -- collisions management with left and right walls
+  if ball.positionX <= 0 or (ball.positionX + ball.width) >= love.graphics.getWidth() then
     ballCentring()
   end
   
-  -- left pad collision
+  -- Pads collision
   -- if ... and - (the bottom of the ball) is under the top of pad - and - the top of the ball is above (the bottom of the pad)
   if ball.positionX <= (padLeft.positionX + padLeft.width) and (ball.positionY + ball.height) > padLeft.positionY and ball.positionY < (padLeft.positionY + padLeft.height) then
     ball.speedX = -ball.speedX
     
     -- ball repositionning
     ball.positionX = padLeft.positionX + padLeft.width
+  end
+  
+  if (ball.positionX + ball.width) >= padRight.positionX  and (ball.positionY + ball.height) > padRight.positionY and ball.positionY < (padRight.positionY + padRight.height) then
+    ball.speedX = -ball.speedX
+    
+    -- ball repositionning
+    ball.positionX = padRight.positionX - ball.width
   end
 end
 
--- pads.lua
+++ pads.lua
@@ -3,9 +3,11 @@ padLeft.positionX = 0
 padLeft.positionY = 0
 padLeft.width = 10
 padLeft.height = 80
+padLeft.speedY = 4
 
 padRight = {}
-padRight.positionX = 789
-padRight.positionY = 519
+padRight.positionX = 0
+padRight.positionY = 0
 padRight.width = 10
 padRight.height = 80
+padRight.speedY = 4
